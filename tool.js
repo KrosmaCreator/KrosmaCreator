642542d78649d6401dc72c54ec7493587c095958
KrosmaCreator <krosmacreator@gmail.com>
2017-01-18 22:34:18 +0000

    Add files via upload

--- tool.js
+++ tool.js
@@ -1,5 +1,24 @@
 window.onload = function () {
 
+    VIGNETTE_X = 66;
+    VIGNETTE_Y = 64;
+    VIGNETTE_SX = 1.87;
+    VIGNETTE_SY = 1.80;
+    CARD_SX = 0.5;
+    CARD_SY = 0.5;
+    AP_X = 54;
+    AP_Y = 40;
+    ATK_X = 308 + 4;
+    ATK_Y = 34 + 8;
+    HP_X = 368 + 4;
+    HP_Y = 34 + 8;
+    MP_X = 346 + 2;
+    MP_Y = 74 + 10;
+    NAME_X = 223;
+    NAME_Y = 314;
+    FAMILY_Y = 526;
+    DESC_Y = 380;
+
     $.fn.extend({
         animateCss: function (animationName) {
             var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
@@ -40,9 +59,6 @@ window.onload = function () {
             cardHP: 0,
             cardMP: 0,
             cardAP: 0,
-            effectsUpcase: ["apparition:", "blessé:", "coup de grâce:", "mort:", "mur",
-                "portée:", "inciblable:", "fin du tour:", "début du tour:", "chef:", "at", "ar", "pa", "pm", "pv", "at"],
-            effects: ["charge", "assome", "portée", "initiative"],
             cardDescription: "",
             showGameVignettes: false,
             showCustomVignette: false
@@ -59,17 +75,20 @@ window.onload = function () {
                     this.ATKText.visible = false;
                     this.HPText.visible = false;
                     this.MPText.visible = false;
+                    this.vignette.y = VIGNETTE_Y - 6;
+                    this.updateCropper();
                 } else {
                     this.ATKText.visible = true;
                     this.HPText.visible = true;
                     this.MPText.visible = true;
+                    this.vignette.y = VIGNETTE_Y;
                 }
                 this.updateCardImage();
             },
             whenRaritySelected: function () {
                 this.updateGemmeImage();
-                if (this.selectedRarity.toLowerCase().match("infinite"))
-                    this.updateCardImage();
+                //if (this.selectedRarity.toLowerCase().match("infinite"))
+                this.updateCardImage();
             },
             whenClassSelected: function () {
                 this.updateCardImage();
@@ -94,31 +113,31 @@ window.onload = function () {
             updateGemmeImage: function () {
                 var name = "img/gemmes/" + this.getGemmeImgName() + ".png";
                 this.gemme.texture = PIXI.loader.resources[name].texture;
-                this.gemme.x = 292 / 2 - this.gemme.width / 2;
-                this.gemme.y = name.includes("peu commune") ? 9 : 7;
+                this.gemme.x = NAME_X - this.gemme.width / 2;
+                this.gemme.y = name.includes("peu commune") ? 22 : 12;
                 this.renderer.render(this.stage);
                 $('#render-div').animateCss('pulse');
             },
             whenAPChanged: function () {
                 this.APText.text = String(this.cardAP);
                 if (this.cardAP > 9)
-                    this.APText.x = 18;
+                    this.APText.x = AP_X - 16;
                 else
-                    this.APText.x = 33;
+                    this.APText.x = AP_X;
                 this.renderer.render(this.stage);
                 $('#render-div').animateCss('pulse');
             },
             whenATKChanged: function () {
                 this.ATKText.text = String(this.cardAtk);
                 if (this.cardAtk > 9) {
-                    this.ATKText.x = 188;
-                    this.ATKText.y = 32;
-                    this.ATKText.style.fontSize = '26px';
+                    this.ATKText.x = ATK_X - 16;
+                    this.ATKText.y = ATK_Y + 10;
+                    this.ATKText.style.fontSize = '42px';
                 }
                 else {
-                    this.ATKText.x = 196;
-                    this.ATKText.y = 20;
-                    this.ATKText.style.fontSize = '34px';
+                    this.ATKText.x = ATK_X;
+                    this.ATKText.y = ATK_Y;
+                    this.ATKText.style.fontSize = '50px';
                 }
                 this.renderer.render(this.stage);
                 $('#render-div').animateCss('pulse');
@@ -126,14 +145,14 @@ window.onload = function () {
             whenHPChanged: function () {
                 this.HPText.text = String(this.cardHP);
                 if (this.cardHP > 9) {
-                    this.HPText.x = 228;
-                    this.HPText.y = 32;
-                    this.HPText.style.fontSize = '26px';
+                    this.HPText.x = HP_X - 16;
+                    this.HPText.y = HP_Y + 10;
+                    this.HPText.style.fontSize = '42px';
                 }
                 else {
-                    this.HPText.x = 236;
-                    this.HPText.y = 20;
-                    this.HPText.style.fontSize = '34px';
+                    this.HPText.x = HP_X;
+                    this.HPText.y = HP_Y;
+                    this.HPText.style.fontSize = '50px';
                 }
                 this.renderer.render(this.stage);
                 $('#render-div').animateCss('pulse');
@@ -141,29 +160,29 @@ window.onload = function () {
             whenMPChanged: function () {
                 this.MPText.text = String(this.cardMP);
                 if (this.cardMP > 9) {
-                    this.MPText.x = 212;
-                    this.MPText.y = 60;
-                    this.MPText.style.fontSize = '26px';
+                    this.MPText.x = MP_X - 18;
+                    this.MPText.y = MP_Y + 10;
+                    this.MPText.style.fontSize = '42px';
                 }
                 else {
-                    this.MPText.x = 220;
-                    this.MPText.y = 48;
-                    this.MPText.style.fontSize = '38px';
+                    this.MPText.x = MP_X;
+                    this.MPText.y = MP_Y;
+                    this.MPText.style.fontSize = '50px';
                 }
                 this.renderer.render(this.stage);
                 $('#render-div').animateCss('pulse');
             },
             whenNameChanged: function () {
                 this.NameText.text = this.cardName;
-                this.NameText.x = 150 - this.NameText.width / 2;
-                this.NameText.y = 200;
+                this.NameText.x = NAME_X - this.NameText.width / 2;
+                this.NameText.y = NAME_Y;
                 this.renderer.render(this.stage);
                 $('#render-div').animateCss('pulse');
             },
             whenFamilyChanged: function () {
                 this.FamilyText.text = this.cardFamily;
-                this.FamilyText.x = 150 - this.FamilyText.width / 2;
-                this.FamilyText.y = 336;
+                this.FamilyText.x = NAME_X - this.FamilyText.width / 2;
+                this.FamilyText.y = FAMILY_Y;
                 this.renderer.render(this.stage);
                 $('#render-div').animateCss('pulse');
             },
@@ -264,13 +283,28 @@ window.onload = function () {
                 var name = dir + "v (" + index + ").png";
                 this.vignette.texture = PIXI.loader.resources[name].texture;
                 this.vignette.visible = true;
+                this.lastVignetteIndex = index;
+                this.updateCropper();
                 this.renderer.render(this.stage);
                 $('#render-div').animateCss('pulse');
+            },
+            updateCropper: function() {
+                app.cropper.visible = false;
+                this.vignette.mask = null;
+                if (this.selectedType === "Sort") {
+                    app.cropper.position.x = this.vignette.x;
+                    app.cropper.position.y = this.vignette.y;
+                    app.cropper.scale.x = this.vignette.scale.x;
+                    app.cropper.scale.y = this.vignette.scale.y;
+                    app.cropper.visible = true;
+                    this.vignette.mask = app.cropper;
+                }
             },
             cantLoadVignettes: function () {
                 return !!this.vignettesLoaded[this.selectedVignetteCateg.toLowerCase()];
             },
             whenVignetteCoordsChanged: function () {
+                this.updateCropper();
                 this.renderer.render(this.stage);
             },
             whenDescriptionChanged: function () {
@@ -284,14 +318,13 @@ window.onload = function () {
                     var words = line.split(" ");
                     var lineTxts = [];
                     words.forEach((word) => {
-                        var bold = this.effectsUpcase.includes(word.toLowerCase()) || this.effects.includes(word.toLowerCase());
-                        if (this.effectsUpcase.includes(word))
-                            word = word.toUpperCase();
+                        var bold = word.startsWith("*") && word.endsWith("*");
+                        word = word.replace("*", "").replace("*", "");
                         var txt = new PIXI.Text(word, this.descriptionStyle(bold));
-                        txt.x = 145 + x;
-                        txt.y = 250 + y;
+                        txt.x = NAME_X + x;
+                        txt.y = DESC_Y + y;
                         this.descriptionContainer.addChild(txt);
-                        x += txt.width + 4;
+                        x += txt.width + 6;
                         lineTxts.push(txt);
                     });
                     var lineWidth = 0;
@@ -301,7 +334,7 @@ window.onload = function () {
                     lineTxts.forEach((txt) => {
                         txt.x -= lineWidth / 2;
                     });
-                    y += 14 + 4;
+                    y += 16 + 6;
                 });
                 this.renderer.render(this.stage);
                 $('#render-div').animateCss('pulse');
@@ -309,9 +342,9 @@ window.onload = function () {
             descriptionStyle: function (bold) {
                 return {
                     fontFamily: 'Montserrat',
-                    fontSize: '16px',
-                    fontWeight: bold ? 'bold' : 'normal',
-                    fill: '#000000'
+                    fontSize: '28px',
+                    fontWeight: bold ? 'bold' : '400',
+                    fill: '#000'
                 };
             },
             generate: function () {
@@ -324,7 +357,7 @@ window.onload = function () {
         }
     });
 
-    app.renderer = PIXI.autoDetectRenderer(292, 370, { transparent: true });
+    app.renderer = PIXI.autoDetectRenderer(446, 566, { transparent: true });
     app.stage = new PIXI.Container();
     document.getElementById("render-div").appendChild(app.renderer.view);
     var imgsToLoad = [];
@@ -347,6 +380,7 @@ window.onload = function () {
     imgsToLoad.push("img/gemmes/gemme_infinite nv1.png");
     imgsToLoad.push("img/gemmes/gemme_infinite nv2.png");
     imgsToLoad.push("img/gemmes/gemme_infinite nv3.png");
+    imgsToLoad.push("img/vignettes/mask.png");
     PIXI.loader
         .add(imgsToLoad)
         .on('progress', (l, loadedResource) => {
@@ -359,19 +393,24 @@ window.onload = function () {
             PIXI.loader.resources["img/cartes/neutre_créature_commune.png"].texture
         );
         app.gemme = new PIXI.Sprite();
+        app.cropper = new PIXI.Sprite(
+            PIXI.loader.resources["img/vignettes/mask.png"].texture
+        );
+        app.cropper.visible = false;
         app.descriptionContainer = new PIXI.Container();
 
         var style = {
-            fontFamily: 'Montserrat',
-            fontSize: '50px',
+            fontFamily: 'BERNHC',
+            fontSize: '72px',
             fontWeight: 'bold',
             fill: '#FFF',
             stroke: '#04849A',
-            strokeThickness: 5
+            strokeThickness: 7
         };
         app.APText = new PIXI.Text("0", style);
-        style.fontSize = '34px';
+        style.fontSize = '52px';
         style.stroke = "#895A00";
+        style.strokeThickness = 5;
         app.ATKText = new PIXI.Text("0", style);
         style.stroke = "#8C0110";
         app.HPText = new PIXI.Text("0", style);
@@ -379,16 +418,20 @@ window.onload = function () {
         app.MPText = new PIXI.Text("0", style);
 
         style = {
-            fontFamily: 'Montserrat',
-            fontSize: '20px',
+            fontFamily: 'BERNHC',
+            fontSize: '36px',
             fontWeight: 'bold',
-            fill: '#FFF'
+            fill: '#FFF',
+            stroke: '#000',
+            strokeThickness: 1,
+            letterSpacing: 1,
         };
         app.NameText = new PIXI.Text("", style);
-        style.fontSize = "16px";
+        style.fontSize = "28px";
         app.FamilyText = new PIXI.Text("", style);
 
         app.stage.addChild(app.vignette);
+        app.stage.addChild(app.cropper);
         app.stage.addChild(app.card);
         app.stage.addChild(app.gemme);
         app.stage.addChild(app.APText);
@@ -399,20 +442,22 @@ window.onload = function () {
         app.stage.addChild(app.FamilyText);
         app.stage.addChild(app.descriptionContainer);
 
-        app.vignette.x = 43;
-        app.vignette.y = 41;
-        app.vignette.scale.x = 1.19;
-        app.vignette.scale.y = 1.19;
-        app.card.scale.x = 0.32;
-        app.card.scale.y = 0.32;
-        app.APText.x = 29; //33
-        app.APText.y = 20; //22
-        app.ATKText.x = 196;
-        app.ATKText.y = 20;
-        app.HPText.x = 236;
-        app.HPText.y = 20;
-        app.MPText.x = 220;
-        app.MPText.y = 48;
+        app.vignette.x = VIGNETTE_X;
+        app.vignette.y = VIGNETTE_Y;
+        app.vignette.scale.x = VIGNETTE_SX;
+        app.vignette.scale.y = VIGNETTE_SY;
+        app.card.scale.x = CARD_SX;
+        app.card.scale.y = CARD_SY;
+        app.APText.x = AP_X;
+        app.APText.y = AP_Y;
+        app.ATKText.x = ATK_X;
+        app.ATKText.y = ATK_Y;
+        app.HPText.x = HP_X;
+        app.HPText.y = HP_Y;
+        app.MPText.x = MP_X;
+        app.MPText.y = MP_Y;
+        app.gemme.scale.x = 1.2;
+        app.gemme.scale.y = 1.2;
 
         app.updateCardImage();
         app.updateGemmeImage();
